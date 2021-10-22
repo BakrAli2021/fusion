a10c6bdf6b6c7a4c752558d28baa4d744b5b2a21
BakrAli2021 <87548560+BakrAli2021@users.noreply.github.com>
2021-10-22 16:56:26 +0400

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,31 @@
+$(function() {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        dotsEach: true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 500,
+    });
+});
+
+let inputs = document.querySelectorAll('section.contact form input');
+inputs.forEach((input) => {
+    input.addEventListener('focus', () => {
+        input.previousElementSibling.classList.add('move');
+    });
+    input.addEventListener('blur', () => {
+        input.previousElementSibling.classList.remove('move');
+    });
+});
+window.onscroll = () => {
+    if (window.scrollY > 500) {
+        document.querySelector('div.top').style.opacity = '1';
+    } else {
+        document.querySelector('div.top').style.opacity = '0';
+    }
+}
+
+////// AOS animation
+AOS.init();
